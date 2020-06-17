274096560b49974fb2a91ddb28733c190950931f
sergpsw <sergpsw@gmail.com>
2020-06-17 09:50:38 +0300

    Changed index.php

--- docker/www/index.php
+++ docker/www/index.php
@@ -9,14 +9,39 @@ $link = mysqli_connect($host, $user, $password, $database)
 
 $query ="SELECT * FROM users";
 $result = mysqli_query($link, $query) or die("Error! " . mysqli_error($link)); 
+
 if($result)
 {
-        while ($table = mysqli_fetch_assoc($result)) 
-        {
-                print_r($table);
-                echo "<br>";
-        }
+	echo '<table border="1">';
+	echo '<thead>';
+	echo '<tr>';
+	echo '<th>id</th>';
+	echo '<th>username</th>';
+	echo '<th>email</th>';
+	echo '<th>password</th>';
+	echo '<th>role</th>';
+	echo '<th>created</th>';
+	echo '<th>updated</th>';
+	echo '</tr>';
+	echo '</thead>';
+	echo '<tbody>';
+
+	while ($table = mysqli_fetch_array($result)) 
+	{
+	    echo '<tr>';
+	    echo '<td>' . $table['id'] . '</td>';
+	    echo '<td>' . $table['username'] . '</td>';
+	    echo '<td>' . $table['email'] . '</td>';
+	    echo '<td>' . $table['password'] . '</td>';
+	    echo '<td>' . $table['role'] . '</td>';
+	    echo '<td>' . $table['created'] . '</td>';
+	    echo '<td>' . $table['updated'] . '</td>';
+	    echo '</tr>';
+	}
+	echo '</tbody>';
+	echo '</table>';
 }
+
 
 mysqli_close($link);
 ?>
